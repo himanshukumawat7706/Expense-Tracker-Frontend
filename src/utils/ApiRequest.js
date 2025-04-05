84ac2c7163d0eec2652e9e7dbbc6af146c2c22d7
himanshukumawat7706 <your-email@example.com>
2025-04-05 16:30:05 +0530

    deployment: 2023-10-04 12:00:00

--- src/utils/ApiRequest.js
+++ src/utils/ApiRequest.js
@@ -1,5 +1,6 @@
 
-const host = "http://localhost:5000"
+// const host = "http://localhost:5000";
+const host = "https://hketback.vercel.app";
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
 export const loginAPI = `${host}/api/auth/login`;
