06bbf00839a11aa387c2f630bc37e198028e13c4
himanshukumawat7706 <your-email@example.com>
2025-04-06 21:30:02 +0530

    commit

--- src/utils/ApiRequest.js
+++ src/utils/ApiRequest.js
@@ -1,6 +1,6 @@
 
 // const host = "http://localhost:5000";
-const host = "https://hketback.vercel.app";
+const host = "https://expense-tracker-backend-nine-lake.vercel.app";
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
 export const loginAPI = `${host}/api/auth/login`;
